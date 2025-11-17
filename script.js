12310feb13dcc75b08170dbe58a3887eeda230b2
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-11-17 22:55:45 +0100

    ajouter dashboard et l image de espace workers

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+///////////Close - Display modal function
+const modal = document.querySelector(".modal");
+
+function Closemodal() {
+    modal.classList.add("invisible")
+}
+function Displaymodal() {
+    modal.classList.remove("invisible")
+}
+
+
