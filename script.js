59b75ce30295cf843a1ae15ef33e8beaa1bbc89a
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-11-18 20:33:35 +0100

    ajouter le modal avec blur background

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-
+workers=[];
 ///////////Close - Display modal function
 const modal = document.querySelector(".modal");
-
+const blureddiv = document.querySelector(".bg-blur");
 function Closemodal() {
-    modal.classList.add("invisible")
+  modal.classList.add("invisible");
+  blureddiv.classList.add("invisible");
 }
 function Displaymodal() {
-    modal.classList.remove("invisible")
-}
+  modal.classList.remove("invisible");
 
+  blureddiv.classList.remove("invisible");
+}
 
